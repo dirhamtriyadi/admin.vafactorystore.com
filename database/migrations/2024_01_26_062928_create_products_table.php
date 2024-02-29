f1ce73fa34042ad384bca646c5526fcbe76bf340
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-02-29 19:15:05 +0700

    fix: fix error upload product to wordpress

--- database/migrations/2024_01_26_062928_create_products_table.php
+++ database/migrations/2024_01_26_062928_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('woocommerce_id')->nullable();
             $table->string('code')->unique();
             $table->string('name');
             $table->string('description')->nullable();
