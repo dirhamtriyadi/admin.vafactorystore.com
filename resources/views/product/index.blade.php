7003007c29f7245d3fcb0c415b287aa099794564
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-05-18 13:08:42 +0700

    refactor: remove unused modal

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -137,31 +137,6 @@
                 </div>
             </div>
         </div>
-
-        {{-- Modal --}}
-        <div class="modal fade" id="modal-lg" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Large Modal</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <img src="" alt="product" id="image" width="100%">
-                        {{-- <p>One fine body…</p> --}}
-                    </div>
-                    <div class="modal-footer justify-content-end">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                    {{-- <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div> --}}
-                </div>
-            </div>
-        </div>
     </section>
 @endsection
 
